b154ed891a3c91836c4d5351c0fb8e5ef2d97be3
user <wodndhksk@gmail.com>
2022-07-22 14:24:15 +0900

    contents api 생성

--- mypj-api/src/main/java/com/example/mypj/content/ContentsApiController.java
+++ mypj-api/src/main/java/com/example/mypj/content/ContentsApiController.java
@@ -5,10 +5,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Member;
 import java.util.List;
@@ -20,9 +17,15 @@ public class ContentsApiController {
 
     private final ContentsServiceImpl contentsService;
 
-    @GetMapping("/find-by-title")
-    public ResponseEntity<List<Contents>> getAccountList(){
+    @GetMapping("/all")
+    public ResponseEntity<List<Contents>> getContentList(){
         List<Contents> contentList = contentsService.contentList();
+        return ResponseEntity.ok().body(contentList);
+    }
+
+    @PostMapping("/find-by-title")
+    public ResponseEntity<List<Contents>> getSearchContents(@RequestParam String title){
+        List<Contents> contentList = contentsService.findByTitle(title);
         return ResponseEntity.ok().body(contentList);
     }
 }
